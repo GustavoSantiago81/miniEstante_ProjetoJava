9d8623f6a05ae17e12879dcb00fbcc77194fa32f
becadfd <damas.rebeca@outlook.com>
2023-02-20 18:18:31 -0300

    Lista dos Livros

--- miniEstante/src/miniEstante/LivrosDoMes/LivrosDoMes.java
+++ miniEstante/src/miniEstante/LivrosDoMes/LivrosDoMes.java
@@ -3,21 +3,29 @@ package miniEstante.LivrosDoMes;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import miniEstante.paginaCliente.paginaCliente;
-
 public class LivrosDoMes {
-
-	public static void livrosDoMes() {
-
-		System.out.println("lista de livros");
-
-	}
 
 	static Scanner leia = new Scanner(System.in);
 
 	public static ArrayList<String> livros = new ArrayList<String>();
 
 	public static String visualizarLivros() {
+
+		String[][] livros = { { "LINS, Armando", "Desvendando as Collections", "2018" },
+				{ "ARAÚJO, Fernanda", "Matrizes: O que realmente são?", "2019" },
+				{ "MENDES, Pedro", "Como usar o Git Bash", "2021" },
+				{ "NEVES, Bruna", "Revelando a Linguagem Por Trás do Seu Aplicativo", "2020" },
+				{ "LIMA, Gabriela", "Codar, Rezar, Rodar", "2019" } };
+
+		System.out.println("Livros do mês: \n");
+		for (String[] livro : livros) {
+			System.out.println(livro[0] + " - " + livro[1] + " (" + livro[2] + ")\n");
+		}
+
+		// Cria um array de arrays de Strings contendo informações sobre os livros.
+
+		// Exibe os livros da lista, um por linha
+
 		return ("");
 	}
 
@@ -36,10 +44,10 @@ public class LivrosDoMes {
 			else if (!livro.equals("0")) {
 				livros.add(livro);
 				System.out.println("Livro adicionado!\n");
-			}		
+			}
 		} while (!livro.equals("0"));
 
-		//HomePage.homePage();
+		// HomePage.homePage();
 	}
 
 }
--- miniEstante/src/miniEstante/paginaCliente/paginaCliente.java
+++ miniEstante/src/miniEstante/paginaCliente/paginaCliente.java
@@ -35,7 +35,7 @@ public class paginaCliente {
 			
 			switch (opcao) {
 			case 1 -> System.out.println("1");
-			case 2 -> LivrosDoMes.livrosDoMes();
+			case 2 -> LivrosDoMes.visualizarLivros();
 			case 3 -> devolucaoLivro.devolver();
 			case 4 -> System.out.println("3");
 			case 5 -> Menu.main(null);
