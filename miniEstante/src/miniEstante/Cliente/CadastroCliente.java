3aaded4adb5bb831c9ef40a7638288af53a33c49
Laise France <laise94@hotmail.com>
2023-02-20 19:18:40 -0300

    ajustes

--- miniEstante/src/miniEstante/Cliente/CadastroCliente.java
+++ miniEstante/src/miniEstante/Cliente/CadastroCliente.java
@@ -33,7 +33,7 @@ public class CadastroCliente {
 				int contador = 3;
 
 				do {
-
+listarClientes();
 					int i = CadastroCliente.emails.indexOf(email);
 
 					System.out.println("Digite a senha:");
