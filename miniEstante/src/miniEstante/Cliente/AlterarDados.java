4ec11ed33f0a692f3a491bed82bbc4d7beb5b27e
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-02-21 22:40:59 -0300

    Com senha após terminar todas alterações | Da erro quando muda varias de uma vez

--- miniEstante/src/miniEstante/Cliente/AlterarDados.java
+++ miniEstante/src/miniEstante/Cliente/AlterarDados.java
@@ -2,24 +2,36 @@ package miniEstante.Cliente;
 
 import java.util.Scanner;
 
+import miniEstante.paginaCliente.paginaCliente;
+
 public class AlterarDados {
 
 	static Scanner leia = new Scanner(System.in);
+
+	private static String email;
+	private static String nome;
+	private static String senha;
+	private static String telefone;
+	private static int set = 0, contador = 3;
 
 	public static void alterarDados() {
 		int opcao;
 		int i = CadastroCliente.emails.indexOf(CadastroCliente.getEmail());
 
+		email = CadastroCliente.getEmail();
+		nome = CadastroCliente.nomes.get(i);
+		telefone = CadastroCliente.telefones.get(i);
+
 		do {
 
 			System.out.println("1 - Email:");
-			System.out.println(CadastroCliente.getEmail());
-			System.out.println("2 - Nome");
-			System.out.println(CadastroCliente.nomes.get(i));
-			System.out.println("3 - Senha");
+			System.out.println(email);
+			System.out.println("2 - Nome:");
+			System.out.println(nome);
+			System.out.println("3 - Senha:");
 			System.out.println("******");
 			System.out.println("4 - Telefone:");
-			System.out.println(CadastroCliente.telefones.get(i));
+			System.out.println(telefone);
 			System.out.println("0 - Voltar");
 			System.out.println("5 - Salvar");
 
@@ -30,27 +42,58 @@ public class AlterarDados {
 			case 1 -> {
 				System.out.println("Digite o novo Email: ");
 				leia.skip("\\R?");
-				String email = leia.nextLine();
-				CadastroCliente.emails.set(i, email);
+				email = leia.nextLine();
 			}
 			case 2 -> {
 				System.out.println("Digite o novo Nome: ");
 				leia.skip("\\R?");
-				String nome = leia.nextLine();
-				CadastroCliente.emails.set(i, nome);
+				nome = leia.nextLine();
 			}
 			case 3 -> {
 				System.out.println("Digite a nova senha: ");
 				leia.skip("\\R?");
-				String senha = leia.nextLine();
-				CadastroCliente.emails.set(i, senha);
+				senha = leia.nextLine();
 			}
 			case 4 -> {
 				System.out.println("Digite o novo Telefone: ");
 				leia.skip("\\R?");
-				String telefone = leia.nextLine();
-				CadastroCliente.emails.set(i, telefone);
+				telefone = leia.nextLine();
 			}
+			case 5 -> {
+
+				do {
+
+					int numeroEmail = CadastroCliente.emails.indexOf(CadastroCliente.getEmail());
+
+					System.out.println("Digite a senha:");
+					leia.skip("\\R?");
+					senha = leia.nextLine();
+
+					if (senha.isBlank())
+						System.out.println("Senha Vazia!\n");
+					else if (senha.equals(CadastroCliente.senhas.get(numeroEmail))) {
+
+						CadastroCliente.emails.set(i, email);
+						CadastroCliente.nomes.set(i, nome);
+						CadastroCliente.senhas.set(i, senha);
+						CadastroCliente.telefones.set(i, telefone);
+
+						contador = -1;
+
+					} else {
+						System.out.println("Senha incorreta!");
+
+							if (contador > 0) {
+								System.out.println("Você tem " + contador + " tentativa(s)!");
+							}
+						
+						contador--;
+					}
+
+				} while (contador >= 0);
+
+			}
+
 			default -> System.out.println("Opção inválida");
 			}
 
